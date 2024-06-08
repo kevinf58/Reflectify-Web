1255cc22e98d1d9a042dc7e45ada28234bf7fe8d
kevin <kevinfng12@gmail.com>
2024-06-08 00:24:32 -0400

    another workflow test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { PrimaryNavBar } from "./components/NavBar/PrimaryNavBar";
+import { SecondaryNavBar } from "./components/NavBar/SecondaryNavBar";
+import { Router } from "./Router";
+import { Footer } from "./components/common/Footer";
+
 function App() {
-  return <div>Hellowwww world!</div>;
+  return (
+    <>
+      <section className="flex flex-col min-h-screen">
+        
+        <Footer />
+      </section>
+    </>
+  );
 }
 
 export default App;
