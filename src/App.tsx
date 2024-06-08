b8c318d94d654b70a00d40c3b5755c55fcb22e33
kevin <kevinfng12@gmail.com>
2024-06-08 00:25:40 -0400

    another test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { PrimaryNavBar } from "./components/NavBar/PrimaryNavBar";
-import { SecondaryNavBar } from "./components/NavBar/SecondaryNavBar";
-import { Router } from "./Router";
 import { Footer } from "./components/common/Footer";
 
 function App() {
   return (
     <>
       <section className="flex flex-col min-h-screen">
-        
         <Footer />
       </section>
     </>
