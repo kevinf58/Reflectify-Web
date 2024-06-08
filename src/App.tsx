cb2199b96916843014a87998b343a6ff60aae68c
kevin <kevinfng12@gmail.com>
2024-06-08 00:21:03 -0400

    testing workflow once again

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
-import { PrimaryNavBar } from "./components/NavBar/PrimaryNavBar";
-import { SecondaryNavBar } from "./components/NavBar/SecondaryNavBar";
-import { Router } from "./Router";
-import { Footer } from "./components/common/Footer";
-
 function App() {
-  return (
-    <>
-      <section className="flex flex-col min-h-screen">
-        <header className="sticky top-0 z-10">
-          <PrimaryNavBar />
-          <SecondaryNavBar />
-        </header>
-        <main className="grow">
-          <Router />
-        </main>
-        <Footer />
-      </section>
-    </>
-  );
+  return <div>Hellowwww world!</div>;
 }
 
 export default App;
